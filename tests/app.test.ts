7db4501f3ce6055270caced5054d1a86bba6e0f7
SelrahcD <c.desneuf@gmail.com>
2025-02-06 13:42:10 +0100

    refactor: Extract assertion in TodoListApp class

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -18,12 +18,20 @@ describe('Todo list app', () => {
 
     describe('List tasks in todo list', () => {
         it('when a list is created, it\'s an empty list', async () => {
-            const response = await request(app)
-                .get('/list')
-                .expect('Content-Type', /json/)
-                .expect(200);
-
-            expect(response.body).toEqual({ tasks: [] });
+            const todoListApp = new TodoListApp()
+            await todoListApp.assertTaskListIsEmpty();
         });
     });
 });
+
+class TodoListApp {
+    async assertTaskListIsEmpty() {
+        const response = await request(app)
+            .get('/list')
+            .expect('Content-Type', /json/)
+            .expect(200);
+
+        expect(response.body).toEqual({tasks: []});
+    }
+
+}
